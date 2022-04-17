2101fd0e067d3453c8b929142d64a3e951fbb1fd
beatriz.nnds <beatriz.nnds@gmail.com>
2022-04-16 21:40:24 -0300

    git commit -tudo funcionando DAQUELE jeito

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ let nome = "";
 let mensagemEnviar;
 let res;
 let mensagensEnviadas = [];
-
+let enderecoAPI = "https://mock-api.driven.com.br/api/v6/uol/";
+let mensagensStatus;
+let mensagensChat;
 
 
 function pedirNome () {
@@ -24,21 +26,32 @@ function ErroNome (error) {
 pedirNome();
 
 function mensagemStatus () {
-    const mensagensStatus = document.querySelector(".mensagem-status");
-    mensagensStatus.innerHTML = "";
-    for (let i=0; i < mensagensStatus.length; i++) {
-        mensagensStatus.innerHTML += `
-            <p class="horario">
-                <span>${resposta.data.time[i]}</span>            
-            </p>
-            <p class="mensagem-usuario">
-                <span>${resposta.data.name[i]}</span>
-            </p>
-            <p class="status-usuario">
-                <span>entra na sala...</span>
-            </p>`
-    }
+    const promiseStatus = axios.get(`${enderecoAPI}messages`);
+    promiseStatus.then(res => {
+        mensagensStatus = document.querySelector(".box");
+        mensagensStatus.innerHTML = "";
+        for (let i=0; i < res.data.length; i++) {
+            mensagensStatus.innerHTML += `
+                <div class="box">
+                    <div class="mensagem-status>
+                        <p class="horario">
+                            <span>(${res.data[i].time})</span>            
+                        </p>
+                        <p class="mensagem-usuario">
+                            <span>${res.data[i].from}</span>
+                        </p>
+                        <p class="status-usuario">
+                            <span>entra na sala...</span>
+                        </p>
+                </div>
+                </div>`
+        }
+    })
+
+    let ultimaMensagem = mensagensStatus.lastChild;
+    ultimaMensagem.scrollIntoView();
 }
+
 
 mensagemStatus();
 
@@ -52,47 +65,38 @@ function procurarParticipantes () {
     promiseParticipantes.then(res => {console.log(res.data)})
 }
 
+procurarParticipantes();
 
 function carregarMensagens () {
     const promiseMensagens = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
-    promiseMensagens.then(processarResposta);
+    promiseMensagens.then (res => {
+        mensagensChat = document.querySelector(".mensagem-normais");
+        mensagensChat.innerHTML = "";
+        for (let i=0; i < res.data.length; i++) {
+            mensagensChat.innerHTML += `
+            <div class="mensagem-normais">
+                <p class="horario">
+                    <span>${resposta.data[i].time}                
+                    </span>
+                </p>
+                <p class="mensagem-usuario">
+                    <span>${resposta.data[i].name}</span>
+                </p>
+                <p class="status-usuario">
+                    <span>para</span>
+                </p>
+                <p class="mensagem-usuario">
+                    <span>${resposta.data[i].for}</span>
+                </p>
+                <p class="status-usuario">
+                    <span>${resposta.data[i].text}</span>
+                </p>
+            </div>`
+        }
+    })
 }
 
-
-function processarResposta (resposta) {
-    mensagensEnviadas = resposta.data;
-    const time = resposta.data.time[i];
-    const from = resposta.data.from[i];
-    const to = resposta.data.to[i];
-    const text = resposta.data.text[i];
-    const type = resposta.data.type[i];
-
-    console.log(mensagensEnviadas);
-}
-
-function carregarChat () {
-    const mensagensGerais = document.querySelector(".mensagem-normais");
-    mensagensGerais.innerHTML = "";
-    for (let i =0; i < mensagensEnviadas.length; i++) {
-        mensagensGerais.innerHTML +=     `      
-            <p class="horario">
-                <span>${resposta.data.time[i]}                
-                </span>
-            </p>
-            <p class="mensagem-usuario">
-                <span>${resposta.data.name[i]}</span>
-            </p>
-            <p class="status-usuario">
-                <span>para</span>
-            </p>
-            <p class="mensagem-usuario">
-                <span>${resposta.data.for[i]}</span>
-            </p>
-            <p class="status-usuario">
-                <span>${resposta.data.text[i]}</span>
-            </p>`
-    }
-}
+carregarMensagens();
 
 function enviarMensagem () {
     mensagemEnviar = {
